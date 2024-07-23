2f446037239c65afc4c4090e7c158de1683d1066
Mautasim <mautasim.1919@gmail.com>
2024-07-23 00:22:07 -0400

    Monday work

--- js/script.js
+++ js/script.js
@@ -1,20 +1,46 @@
 let screen = document.querySelector("h1")
-
+let leftOperand = "0", operator = null, rightOperand = null
 let buttons = document.querySelectorAll("button")
 
 buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
         if (btn.className === "") {
-            screen.innerText += btn.value
+            if (operator === null) {
+                screen.innerText = (leftOperand === "0") ? (btn.value) : (leftOperand + btn.value)
+            } else {
+                screen.innerText = (rightOperand === "0" || rightOperand === null) ? (btn.value) : (rightOperand + btn.value)
+            }
         }
-        else if (btn.className === "operator") {
 
+        else if (btn.className === "operator") {
+            if (rightOperand !== null) {
+                if (btn.value === "+") {
+                    screen.innerText = parseFloat(leftOperand) + parseFloat(screen.innerText)
+                } else if (btn.value === "-") {
+                    screen.innerText = parseFloat(leftOperand) - parseFloat(screen.innerText)
+                } else if (btn.value === "*") {
+                    screen.innerText = parseFloat(leftOperand) * parseFloat(screen.innerText) 
+                } else {
+                    if (screen.innerText === "0") {
+                        screen.innerText = "Infinity"
+                        leftOperand = null 
+                    } else {
+                        screen.innerText = parseFloat(leftOperand) / parseFloat(screen.innerText)
+                    }
+                }
+                if (screen.innerText !== "Infinity") {
+                    leftOperand = screen.innerText
+                }
+                rightOperand = null 
+            }
+            operator = btn.value 
+            leftOperand = screen.innerText
         }
         else if (btn.className === "decimal") {
-
+            screen.innerText = (screen.innerText.includes(".")) ? screen.innerText : (screen.innerText + ".")
         }
         else if (btn.className === "clear") {
-
+            screen.innerText = "0"
         }
         else if (btn.className === "equal-sign operator") {
 
